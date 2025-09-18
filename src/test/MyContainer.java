b5bf033818184aad2cb77ed51766855056d0a394
Anton Ovchynnikov <anton.ovchynnikov13@gmail.com>
2025-09-18 21:57:08 +0200

    feat: select cell on mouse click and update window title(task 2)

--- src/test/MyContainer.java
+++ src/test/MyContainer.java
@@ -3,45 +3,58 @@ package test;
 import java.awt.Color;
 import java.awt.Font;
 import java.awt.GridLayout;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
 
 import javax.swing.BorderFactory;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.SwingConstants;
+import javax.swing.SwingUtilities;
 
 
 
 public class MyContainer extends JPanel {
 
-	public static final int PANELS_COUNT =16;
+	public static final int PANELS_COUNT = 16;
 	int highlightIndex=6;
 	JLabel[] panels=new JLabel[PANELS_COUNT];
 	SwingTemplate st;
 	public MyContainer(int windowWidth, int windowHeight, SwingTemplate st) {
 		this.setSize(windowWidth, windowHeight);
-		this.st=st;
-		
-		this.setLayout(new GridLayout(4,4));
-		
+		this.st = st;
+
+		this.setLayout(new GridLayout(4, 4));
 
 		Font labelFont = this.getFont();
-		Font myFont=new Font(labelFont.getName(), Font.PLAIN, 30);
-		
-		
+		Font myFont = new Font(labelFont.getName(), Font.PLAIN, 30);
 
-		for (int i=0;i<PANELS_COUNT;i++) {
-			panels[i] =new JLabel();
+		for (int i = 0; i < PANELS_COUNT; i++) {
+			panels[i] = new JLabel();
 			panels[i].setFont(myFont);
 			panels[i].setText(String.valueOf(i));
 			panels[i].setHorizontalAlignment(SwingConstants.CENTER);
+			panels[i].setBorder(BorderFactory.createLineBorder(Color.blue));
+			panels[i].addMouseListener(createClickListener(i));
 			this.add(panels[i]);
 		}
 
-
 		updateView();
-
 	}
-	
+
+	private MouseAdapter createClickListener(int i) {
+		return new MouseAdapter() {
+			@Override
+			public void mouseClicked(MouseEvent e) {
+				if (SwingUtilities.isLeftMouseButton(e)) {
+					highlightIndex = i;
+					updateView();
+				}
+			}
+		};
+	}
+
 	private void updateView() {
 		for (int i=0;i<PANELS_COUNT;i++) {
 			if (i==highlightIndex) {
@@ -52,8 +65,8 @@ public class MyContainer extends JPanel {
 		}
 		st.setTitle("Selected index is " + String.valueOf(highlightIndex));
 	}
-	
-	
+
+
 	public void keyLeft() {
 		if (highlightIndex>0)
 			highlightIndex--;
@@ -67,6 +80,6 @@ public class MyContainer extends JPanel {
 			highlightIndex++;
 
 		updateView();
-	}		
+	}
 
 }
