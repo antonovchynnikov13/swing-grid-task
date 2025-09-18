ce31ed50d31b2f5a856b50b02e81b51bfa7bc488
Anton Ovchynnikov <anton.ovchynnikov13@gmail.com>
2025-09-18 20:55:50 +0200

    chore: initial commit with base Swing application

---
+++ src/test/MyContainer.java
@@ -0,0 +1,72 @@
+package test;
+
+import java.awt.Color;
+import java.awt.Font;
+import java.awt.GridLayout;
+
+import javax.swing.BorderFactory;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.SwingConstants;
+
+
+
+public class MyContainer extends JPanel {
+
+	public static final int PANELS_COUNT =16;
+	int highlightIndex=6;
+	JLabel[] panels=new JLabel[PANELS_COUNT];
+	SwingTemplate st;
+	public MyContainer(int windowWidth, int windowHeight, SwingTemplate st) {
+		this.setSize(windowWidth, windowHeight);
+		this.st=st;
+		
+		this.setLayout(new GridLayout(4,4));
+		
+
+		Font labelFont = this.getFont();
+		Font myFont=new Font(labelFont.getName(), Font.PLAIN, 30);
+		
+		
+
+		for (int i=0;i<PANELS_COUNT;i++) {
+			panels[i] =new JLabel();
+			panels[i].setFont(myFont);
+			panels[i].setText(String.valueOf(i));
+			panels[i].setHorizontalAlignment(SwingConstants.CENTER);
+			this.add(panels[i]);
+		}
+
+
+		updateView();
+
+	}
+	
+	private void updateView() {
+		for (int i=0;i<PANELS_COUNT;i++) {
+			if (i==highlightIndex) {
+				panels[i].setBorder(BorderFactory.createLineBorder(Color.green, 3));
+			}else {
+				panels[i].setBorder(BorderFactory.createLineBorder(Color.blue));
+			}
+		}
+		st.setTitle("Selected index is " + String.valueOf(highlightIndex));
+	}
+	
+	
+	public void keyLeft() {
+		if (highlightIndex>0)
+			highlightIndex--;
+
+		updateView();
+	}
+
+
+	public void keyRight() {
+		if (highlightIndex<PANELS_COUNT-1)
+			highlightIndex++;
+
+		updateView();
+	}		
+
+}
---
+++ src/test/SwingTemplate.java
@@ -0,0 +1,67 @@
+package test;
+
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
+ 
+// Swing Program Template
+@SuppressWarnings("serial")
+public class SwingTemplate extends JFrame implements KeyListener {
+    // Name-constants to define the various dimensions
+    public static final int WINDOW_WIDTH = 600;
+    public static final int WINDOW_HEIGHT = 600; //константи
+    // ......
+
+    // private variables of UI components
+    // ......
+    MyContainer container; //мій контейнер
+
+    /**
+     * Constructor to setup the UI components
+     */
+    public SwingTemplate() {    //конструктор для створення фрейма
+        Container cp = this.getContentPane();   //контейнер для компонентів
+
+        // Content-pane sets layout
+        // cp.setLayout(new ....Layout());
+
+        // Allocate the UI components
+        // .....
+
+        // Content-pane adds components
+        container = new MyContainer(WINDOW_WIDTH, WINDOW_HEIGHT, this); //новий JPanel
+        cp.add(container); //додаємо нашу панель у content pane
+        addKeyListener(this); //реєcструємо фрейм як обробник клави
+
+        // Source object adds listener
+        // .....
+
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);  // Exit when close button clicked
+        //setTitle("......"); // "this" JFrame sets title
+        setSize(WINDOW_WIDTH, WINDOW_HEIGHT);  // or pack() the components
+        setVisible(true);   // show it
+
+    }
+
+    @Override
+    public void keyPressed(KeyEvent arg0) {
+        if (arg0.getKeyCode() == KeyEvent.VK_LEFT) {
+            container.keyLeft();
+        } else if (arg0.getKeyCode() == KeyEvent.VK_RIGHT) {
+            container.keyRight();
+        }
+    }
+
+    @Override
+    public void keyReleased(KeyEvent arg0) {
+        // TODO Auto-generated method stub
+
+    }
+
+    @Override
+    public void keyTyped(KeyEvent arg0) {
+        // TODO Auto-generated method stub
+
+    }
+
+}
