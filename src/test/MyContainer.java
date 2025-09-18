e7a453bec31e4dd6b81c2c427de79a22fdf8494a
Anton Ovchynnikov <anton.ovchynnikov13@gmail.com>
2025-09-19 00:35:21 +0200

    feat: add selection modes (HOVER/CLICK) with toggle by space key and fixed bugs

--- src/test/MyContainer.java
+++ src/test/MyContainer.java
@@ -1,47 +1,81 @@
 package test;
 
-import java.awt.Color;
-import java.awt.Font;
-import java.awt.GridLayout;
+import java.awt.*;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
 
 import javax.swing.BorderFactory;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.SwingConstants;
+import javax.swing.SwingUtilities;
 
 
 
 public class MyContainer extends JPanel {
 
-	public static final int PANELS_COUNT =16;
+	private SelectionMode selectionMode = SelectionMode.HOVER;
+	public static final int PANELS_COUNT = 16;
+	private JLabel modeLabel;
 	int highlightIndex=6;
 	JLabel[] panels=new JLabel[PANELS_COUNT];
 	SwingTemplate st;
+
 	public MyContainer(int windowWidth, int windowHeight, SwingTemplate st) {
 		this.setSize(windowWidth, windowHeight);
-		this.st=st;
-		
-		this.setLayout(new GridLayout(4,4));
-		
+		this.st = st;
+		this.setLayout(new BorderLayout());
+		modeLabel = new JLabel("Mode: " + selectionMode, SwingConstants.CENTER);
+		this.add(modeLabel, BorderLayout.NORTH);
+
+		JPanel gridPanel = new JPanel(new GridLayout(4, 4));
 
 		Font labelFont = this.getFont();
-		Font myFont=new Font(labelFont.getName(), Font.PLAIN, 30);
-		
-		
+		Font myFont = new Font(labelFont.getName(), Font.PLAIN, 30);
 
-		for (int i=0;i<PANELS_COUNT;i++) {
-			panels[i] =new JLabel();
+		for (int i = 0; i < PANELS_COUNT; i++) {
+			panels[i] = new JLabel();
 			panels[i].setFont(myFont);
 			panels[i].setText(String.valueOf(i));
 			panels[i].setHorizontalAlignment(SwingConstants.CENTER);
-			this.add(panels[i]);
+			panels[i].setBorder(BorderFactory.createLineBorder(Color.blue));
+			panels[i].addMouseListener(createSelectionListener(i));
+			gridPanel.add(panels[i]);
 		}
-
-
+		this.add(gridPanel, BorderLayout.CENTER);
 		updateView();
-
 	}
-	
+
+	private MouseAdapter createSelectionListener(int i) {
+		return new MouseAdapter() {
+			@Override
+			public void mouseClicked(MouseEvent e) {
+				if (selectionMode == SelectionMode.CLICK && SwingUtilities.isLeftMouseButton(e)) {
+					highlightIndex = i;
+					updateView();
+				}
+			}
+
+			@Override
+			public void mouseEntered(MouseEvent e) {
+				if (selectionMode == SelectionMode.HOVER) {
+					highlightIndex = i;
+					updateView();
+				}
+			}
+		};
+	}
+
+	public void setSelectionMode(SelectionMode mode) {
+		this.selectionMode = mode;
+		modeLabel.setText("Mode: " + mode);
+	}
+
+	public SelectionMode getSelectionMode() {
+		return selectionMode;
+	}
+
 	private void updateView() {
 		for (int i=0;i<PANELS_COUNT;i++) {
 			if (i==highlightIndex) {
@@ -52,8 +86,8 @@ public class MyContainer extends JPanel {
 		}
 		st.setTitle("Selected index is " + String.valueOf(highlightIndex));
 	}
-	
-	
+
+
 	public void keyLeft() {
 		if (highlightIndex>0)
 			highlightIndex--;
@@ -67,6 +101,6 @@ public class MyContainer extends JPanel {
 			highlightIndex++;
 
 		updateView();
-	}		
+	}
 
 }
